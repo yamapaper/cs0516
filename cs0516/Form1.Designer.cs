3931579ff1397a47681921e9529c1f3977717d64
yamada kenta <yamapaper2234@gmail.com>
2025-05-16 15:56:08 +0900

    変数

--- cs0516/Form1.Designer.cs
+++ cs0516/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(5, 15);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 75);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(5, 264);
+            label1.Name = "label1";
+            label1.Size = new Size(19, 15);
+            label1.TabIndex = 1;
+            label1.Text = "亀";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(5, 96);
+            button2.Name = "button2";
+            button2.Size = new Size(126, 69);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(5, 171);
+            button3.Name = "button3";
+            button3.Size = new Size(131, 90);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(179, 209);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(172, 23);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "山田健太";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(5, 282);
+            button4.Name = "button4";
+            button4.Size = new Size(113, 70);
+            button4.TabIndex = 5;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        public System.Windows.Forms.Timer timer1;
+        private Button button4;
     }
 }
--- cs0516/Form1.cs
+++ cs0516/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace cs0516
 {
     public partial class Form1 : Form
@@ -5,6 +7,75 @@ namespace cs0516
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ç±ÇÒÇ…ÇøÇÕ");
+            MessageBox.Show(100.ToString());
+            MessageBox.Show($"{100}");
+            MessageBox.Show($"label1.Top={label1.Top}");
+            MessageBox.Show(3.14f.ToString());
+            MessageBox.Show($"{3.14f}");
+
+            MessageBox.Show(1 + 1.ToString());//èoóÕÇÕÅu11Åv
+            MessageBox.Show((1 + 1).ToString());
+            MessageBox.Show($"{1 + 1}");
+            MessageBox.Show((1.23f * 2.34).ToString());
+            MessageBox.Show($"{1.23f * 2.34}");
+            MessageBox.Show("1" + "1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+            a += 5;
+            MessageBox.Show($"{a}");
+            a -= 5;
+            MessageBox.Show($"{a}");
+            a *= 5;
+            MessageBox.Show($"{a}");
+            a /= 5;
+            MessageBox.Show($"{a}");
+            a %= 5;
+            MessageBox.Show($"{a}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+            label1.Text = "hello";
+            MessageBox.Show($"Ç±ÇÒÇ…ÇøÇÕ{textBox1.Text}Ç≥ÇÒ");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += 1;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a;
+            float fw;
+            string str;
+            bool bl;
+
+            int b = 10;
+            float fw2 = -10.5f;
+            string str2 = "Ç±ÇÒÇ…ÇøÇÕ";
+            bool bl2 = true;
         }
     }
 }
